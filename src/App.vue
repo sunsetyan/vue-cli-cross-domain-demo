66340bea0ca4f073f50ec1eba370058b3b8fc90d
shengl <sunsetxiao@126.com>
2020-05-31 22:33:52 +0800

    cors:

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div id="app">
+    <button v-on:click="sbt">访问跨域</button>
+
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 export default {
   name: 'App'
 }
+
+new Vue({
+  el: '#app',
+  data: {
+    message: 'abdd'
+  },
+  methods: {
+    sbt: function() {
+      this.axios.get('/api/project/5ed359902945ea2cfa292b98', {
+
+      }).then(function(response){
+        console.log(response);
+      });
+    }
+  }
+})
 </script>
 
 <style>
